f4293575ba83cbe37939438de1c128668c6efe9d
Ethan Hunt <Udshah31@gmail.com>
2021-06-04 14:21:36 +0545

    day-12 complete

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_catlog/models/catelog.dart';
 import 'package:flutter_catlog/widgets/drawer.dart';
 import 'package:flutter_catlog/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
-  final int days = 30;
-  final String name = "Uday";
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodeData = jsonDecode(catalogJson);
+    var productsData = decodeData["products"];
+  }
 
   @override
   Widget build(BuildContext context) {
