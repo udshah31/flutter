878a17456f77299acca7ebcde5140750e3fdbef2
Ethan Hunt <Udshah31@gmail.com>
2021-06-06 08:56:18 +0545

    day-14 complete

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,7 +3,7 @@ import 'package:flutter/services.dart';
 import 'dart:convert';
 import 'package:flutter_catlog/models/catelog.dart';
 import 'package:flutter_catlog/widgets/drawer.dart';
-import 'package:flutter_catlog/widgets/item_widget.dart';
+
 
 class HomePage extends StatefulWidget {
   @override
@@ -38,13 +38,44 @@ class _HomePageState extends State<HomePage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: (CatelogModel.items != null && CatelogModel.items!.isNotEmpty)
-            ? ListView.builder(
-                itemCount: CatelogModel.items!.length,
+            ? GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 16,
+                    crossAxisSpacing: 16),
                 itemBuilder: (context, index) {
-                  return ItemWidget(
-                    item: CatelogModel.items![index],
+                  final item = CatelogModel.items![index];
+                  return Card(
+                    clipBehavior: Clip.antiAlias,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    child: GridTile(
+                      header: Container(
+                        child: Text(
+                          item.name,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        padding: const EdgeInsets.all(12),
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                        ),
+                      ),
+                      child: Image.network(item.image),
+                      footer: Container(
+                        child: Text(
+                          item.price.toString(),
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        padding: const EdgeInsets.all(12),
+                        decoration: BoxDecoration(color: Colors.black),
+                      ),
+                    ),
                   );
-                })
+                },
+                itemCount: CatelogModel.items!.length,
+              )
             : Center(
                 child: CircularProgressIndicator(),
               ),
